8e051bb68c508fa13a32ec4bc65ad54578f55ee2
Daniel Lerner <daniel.lerner2@gmail.com>
2017-09-07 16:07:59 -0400

    sendGameData function added

--- static/js/game.js
+++ static/js/game.js
@@ -10,6 +10,17 @@ var black = {
   score: 0
 };
 
+var sendGameData = function(){
+  var gameData = JSON.stringify({"endTime": 56788, "bScore": 4, "yScore": 5, "bName": "daniel", "yName": "sara", "startTime": 53435, "_id": 3});
+  $.get(
+      url="endgame",
+      data={"gameData":gameData}, 
+      success=function(data) {
+        //  alert('page content: ' + data);
+      }
+  );
+}
+
 function updateScore(yellowScored, blackScored){
   if (yellowScored) {
     yellow.score++
