93d76610ee7fd1b441d604f773bd86bbecf1300d
Sara Stiklickas <sara.stiklickas@ge.com>
2017-09-08 10:37:00 -0400

    Endgame modal appears

--- static/js/game.js
+++ static/js/game.js
@@ -37,13 +37,17 @@ function updateScore(yellowScored, blackScored){
 function setDefaultPlayerName() {
 	var elYellowName = document.getElementById("yellow--name");
 	var elBlackName = document.getElementById("black--name");
+	var modalYellowName = document.getElementById("yellow-name--modal");
+	var modalBlackName = document.getElementById("black-name--modal");
 
 	if (elYellowName.innerText === "") {
 		elYellowName.innerText = yellow.name;
+		modalYellowName.innerText = yellow.name;
 	}
 
 	if (elBlackName.innerText === "") {
 		elBlackName.innerText = black.name;
+		modalBlackName.innerText = black.name;
 	}
 }
 
