9a28a61f429721c7068681c4c45fc226bd9921b2
Brett Oberg <brett.oberg@ge.com>
2017-10-11 16:08:54 -0400

    Removed bakePie comments so the sensor data can be read.

--- app.py
+++ app.py
@@ -7,39 +7,39 @@ import os, random
 app = Flask(__name__)
 
 blackPin, yellowPin = 5, 6
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(blackPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(yellowPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(blackPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(yellowPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 blackScored = False
 yellowScored = False
 thread = Thread()
 
 def bakePie():
-    # GPIO.add_event_detect(blackPin, GPIO.RISING)
-    # GPIO.add_event_detect(yellowPin, GPIO.RISING)
+    GPIO.add_event_detect(blackPin, GPIO.RISING)
+    GPIO.add_event_detect(yellowPin, GPIO.RISING)
     while True:
         global yellowScored
         global blackScored
         yellowScored = False
         blackScored = False
-        # if GPIO.event_detected(yellowPin):
-        #     print "yelow goal!"
-        #     yellowScored = True
-        # elif GPIO.event_detected(blackPin, GPIO.RISING):
-        #     print "black goal"
-        #     blackScored = True
-        # result = random.random()  
-        result = 1
-        # print result
-        if result < 0.5:
-            # print "black goal"
-            blackScored = True
-            time.sleep(1)
-        else:
-            # print "yelow goal!"
+        if GPIO.event_detected(yellowPin):
+            print "yelow goal!"
             yellowScored = True
-            time.sleep(1)
+        elif GPIO.event_detected(blackPin, GPIO.RISING):
+            print "black goal"
+            blackScored = True
+        # # result = random.random()  
+        # result = 1
+        # # print result
+        # if result < 0.5:
+        #     # print "black goal"
+        #     blackScored = True
+        #     time.sleep(1)
+        # else:
+        #     # print "yelow goal!"
+        #     yellowScored = True
+        #     time.sleep(1)
 
 # Returns a string representing the game data to be sent to JSON
 def formatGameData(index, gameData):
@@ -88,7 +88,9 @@ def updateBlackScore():
 
 @app.route('/yellowScore')
 def updateYellowScore():
-    return render_template('game.html', result=None, yellowScored=1, blackScored=0)
+    yellow = int(request.args.get('yellow')) + 1
+    black = int(request.args.get('black'))
+    return render_template('game.html', result=None, yellowScored=yellow, blackScored=black)
 
 @app.route('/status')
 def threadStatus():
